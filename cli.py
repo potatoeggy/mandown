3267c5f6328e7ce9cefdcbbb35437ac2ae742937
eggy <eggyrules@gmail.com>
2022-01-04 11:56:09 -0500

    Resolve mypy issues and clean up downloaders

--- cli.py
+++ cli.py
@@ -4,18 +4,17 @@ app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
+def hello(name: str) -> None:
     typer.echo(f"Hello {name}")
 
 
 if __name__ == "__main__":
     # app()
 
-    from mangadownloader import iohandler
+    import mangadownloader
     from mangadownloader.sources.source_mangasee import MangaSeeSource
 
     kag = MangaSeeSource("https://mangasee123.com/manga/Grand-Blue")
     chapters = kag.get_chapter_list()
     first_chap = kag.get_chapter_image_list(chapters[0])
-    io = iohandler.IOHandler()
-    io.download_chapter(first_chap, "/media/kag")
+    mangadownloader.download_chapter(first_chap, "/media/kag")
