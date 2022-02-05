ac26e4689bc3b0395995f6d6e0f4bd33fd2080a2
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2022-02-05 18:15:32 -0500

    Add image processing (#15)
    Resolves #14.
    
    Add `mandown.processing`, which can perform predetermined operations on image files — currently only rotating images that span multiple pages.
    
    Also add a CLI interface for processing.

---
+++ mandown/processing.py
@@ -0,0 +1,57 @@
+from enum import Enum
+from pathlib import Path
+
+from PIL import Image
+
+
+class Ops(str, Enum):
+    """
+    Constants for Processor.process
+    """
+
+    ROTATE_DOUBLE_PAGES = "rotate_double_pages"
+    """If page width is greater than its height, rotate it 90 degrees."""
+    NO_POSTPROCESSING = "none"
+    """Disable image processing entirely."""
+
+
+class Processor:
+    def __init__(self, image_path: Path | str) -> None:
+        self.image_path = Path(image_path)
+        self.image = Image.open(self.image_path)
+        self.modified = False
+
+    def write(self, filename: Path | str | None = None) -> None:
+        """Save the processed image manually"""
+        self.image.save(filename or self.image_path)
+
+    def process(
+        self, operations: list[Ops], filename: Path | str | None = None
+    ) -> None:
+        """
+        Perform the operations in `operations` on the image in sequence
+        and save it to disk. If `filename` is not None, it will be saved
+        with that filename instead.
+        """
+        if Ops.NO_POSTPROCESSING in operations:
+            return
+
+        for func in operations:
+            try:
+                getattr(self, func)()
+            except AttributeError as err:
+                raise NotImplementedError(
+                    f"{func} is not a valid post-processing function."
+                ) from err
+            except OSError as err:
+                raise OSError(f"Error in {self.image_path}") from err
+
+        if self.modified:
+            # only write to disk if something has actually changed
+            self.write(filename)
+
+    def rotate_double_pages(self) -> None:
+        width, height = self.image.size
+        if width > height:
+            self.image = self.image.rotate(90, expand=1)
+            self.modified = True
