97691462c24d143f4c25fb79bfe9b5c72915b26a
eggy <eggyrules@gmail.com>
2022-02-08 13:08:24 -0500

    processing: Add split double pages

--- mandown/processing.py
+++ mandown/processing.py
@@ -13,19 +13,28 @@ class ProcessOps(str, Enum):
 
     ROTATE_DOUBLE_PAGES = "rotate_double_pages"
     """If page width is greater than its height, rotate it 90 degrees."""
+    SPLIT_DOUBLE_PAGES = "split_double_pages"
+    """If page width is greater than its height, split it in half into two images."""
     NO_POSTPROCESSING = "none"
     """Disable image processing entirely."""
+    TRIM_BORDERS = "trim_borders"
 
 
 class Processor:
-    def __init__(self, image_path: Path | str) -> None:
+    def __init__(self, image_path: Path | str, right_to_left: bool = False) -> None:
         self.image_path = Path(image_path)
         self.image = Image.open(self.image_path)
         self.modified = False
+        self.right_to_left = right_to_left
+
+        self.pending_images: list[tuple[Image.Image, Path]] = []
 
     def write(self, filename: Path | str | None = None) -> None:
         """Save the processed image manually"""
         self.image.save(filename or self.image_path)
+
+        for image, path in self.pending_images:
+            image.save(path)
 
     def process(
         self, operations: list[ProcessOps], filename: Path | str | None = None
@@ -57,6 +66,20 @@ class Processor:
         if width > height:
             self.image = self.image.rotate(90, expand=1)
             self.modified = True
+
+    def split_double_pages(self) -> None:
+        width, height = self.image.size
+        if not width > height:
+            return
+
+        left = self.image.crop((0, 0, int(width / 2), height))
+        right = self.image.crop((int(width / 2), 0, width, height))
+
+        self.image = right if self.right_to_left else left
+        new_image = left if self.right_to_left else right
+
+        new_image_path = self.image_path.with_stem(self.image_path.stem + ".5")
+        self.pending_images.append((new_image, new_image_path))
 
 
 def async_process(data: tuple[Path | str, list[ProcessOps]]) -> None:
