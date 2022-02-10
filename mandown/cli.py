3a054efb92654663e41cda461c1fa68a1d2df0ee
eggy <eggyrules@gmail.com>
2022-02-09 22:18:15 -0500

    cli: Properly pass chapter list to converter when downloading

--- mandown/cli.py
+++ mandown/cli.py
@@ -245,7 +245,13 @@ def download(
         cli_process(image_paths, processing_options)
 
     # convert
-    cli_convert(target_path, convert_to, dest, source.metadata, chapter_range)
+    cli_convert(
+        target_path,
+        convert_to,
+        dest,
+        source.metadata,
+        [(c.title, c.title_sanitised) for c in chapter_range],
+    )
 
 
 def main() -> None:
