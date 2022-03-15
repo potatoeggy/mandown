3926fbbf2d7e7bbfd93283a7b5cb713ba8fbd5ad
eggy <eggyrules@gmail.com>
2022-03-15 14:57:27 -0400

    api: Export process functions

--- mandown/__init__.py
+++ mandown/__init__.py
@@ -1,1 +1,7 @@
-from .api import query, download_chapter, download_chapter_progress
+from .api import (
+    download_chapter,
+    download_chapter_progress,
+    process,
+    process_progress,
+    query,
+)
