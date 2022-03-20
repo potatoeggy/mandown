845995fd90d0558efa1c686f7423d6e41332653b
eggy <eggyrules@gmail.com>
2022-03-20 14:38:48 -0400

    Add undetected chromedriver support
    This really sucks but is needed to get around cloudflare

--- mandown/sources/source_readcomiconline.py
+++ mandown/sources/source_readcomiconline.py
@@ -5,8 +5,8 @@ Source file for readcomiconline.li
 
 import re
 
-import requests
 from bs4 import BeautifulSoup
+from ..iohandler import UndetectedDriver
 
 from ..base import BaseChapter, BaseMetadata
 from .base_source import BaseSource
@@ -19,14 +19,17 @@ class ReadComicOnlineSource(BaseSource):
     def __init__(self, url: str) -> None:
         super().__init__(url)
         self.id = self.url_to_id(url)
+        self.driver = UndetectedDriver()
+        self.title = ""
 
     def fetch_metadata(self) -> BaseMetadata:
         soup = BeautifulSoup(
-            requests.get(f"https://readcomiconline.li/Comic/{self.id}").text,
+            self.driver.get(f"https://readcomiconline.li/Comic/{self.id}"),
             features="lxml",
         )
 
-        title = str(soup.select_one("h3").text)
+        title = str(soup.select_one("a.bigChar").text)
+        self.title = title
         author = [
             str(
                 # this site uses "Various" if there's more than one author
@@ -34,26 +37,31 @@ class ReadComicOnlineSource(BaseSource):
             )
         ]
         genres: list[str] = [str(e.text) for e in soup.select("a[href^='/Genre']")]
-        description = str(soup.select_one("p[style='text-align: justify;']").text)
+        desc_element = soup.select_one("p[style='text-align: justify;']")
+
+        description = str(desc_element.text) if desc_element else ""
         cover = self.domains[0] + str(soup.find("link")["href"])
 
         return BaseMetadata(title, author, self.url, genres, description, cover)
 
     def fetch_chapter_list(self) -> list[BaseChapter]:
         soup = BeautifulSoup(
-            requests.get(f"https://readcomiconline.li/Comic/{self.id}").text,
+            self.driver.get(f"https://readcomiconline.li/Comic/{self.id}"),
             features="lxml",
         )
 
         chapters: list[BaseChapter] = []
-        for e in soup.select("ul.list > li > a"):
+        for e in soup.select("td > a"):
             chapters.append(
-                BaseChapter(next(e.children).text, self.domains[0] + e["href"])
+                BaseChapter(
+                    next(e.children).text.strip()[len(self.title) + 1 :],
+                    self.domains[0] + e["href"],
+                )
             )
-        return chapters
+        return list(reversed(chapters))
 
     def fetch_chapter_image_list(self, chapter: BaseChapter) -> list[str]:
-        text = requests.get(chapter.url).text
+        text = self.driver.get(chapter.url)
 
         images: list[str] = []
         start = 0
