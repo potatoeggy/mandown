03f1df1c552f1876cce8182f87d3159dfb3abce1
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2022-07-11 18:49:10 -0400

    feat: Qt GUI (#38)
    It looks ugly, is not responsive, chapter selection doesn't work, and there are still a couple of crashes — but it's okay
    
    Also switch iohandler to io.

---
+++ mandown/ui/mainwin.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.4
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName("Widget")
+        Widget.resize(800, 600)
+        self.verticalLayout_2 = QVBoxLayout(Widget)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label_4 = QLabel(Widget)
+        self.label_4.setObjectName("label_4")
+
+        self.horizontalLayout_2.addWidget(self.label_4)
+
+        self.text_source = QLineEdit(Widget)
+        self.text_source.setObjectName("text_source")
+
+        self.horizontalLayout_2.addWidget(self.text_source)
+
+        self.button_from_url = QPushButton(Widget)
+        self.button_from_url.setObjectName("button_from_url")
+        icon = QIcon(QIcon.fromTheme("search"))
+        self.button_from_url.setIcon(icon)
+
+        self.horizontalLayout_2.addWidget(self.button_from_url)
+
+        self.button_from_folder = QPushButton(Widget)
+        self.button_from_folder.setObjectName("button_from_folder")
+        icon1 = QIcon(QIcon.fromTheme("folder"))
+        self.button_from_folder.setIcon(icon1)
+
+        self.horizontalLayout_2.addWidget(self.button_from_folder)
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.label_5 = QLabel(Widget)
+        self.label_5.setObjectName("label_5")
+
+        self.horizontalLayout_4.addWidget(self.label_5)
+
+        self.text_dest = QLineEdit(Widget)
+        self.text_dest.setObjectName("text_dest")
+
+        self.horizontalLayout_4.addWidget(self.text_dest)
+
+        self.pushButton_3 = QPushButton(Widget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setIcon(icon1)
+
+        self.horizontalLayout_4.addWidget(self.pushButton_3)
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label_2 = QLabel(Widget)
+        self.label_2.setObjectName("label_2")
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.label_image = QLabel(Widget)
+        self.label_image.setObjectName("label_image")
+
+        self.verticalLayout.addWidget(self.label_image)
+
+        self.label_metadata = QLabel(Widget)
+        self.label_metadata.setObjectName("label_metadata")
+
+        self.verticalLayout.addWidget(self.label_metadata)
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.label = QLabel(Widget)
+        self.label.setObjectName("label")
+
+        self.verticalLayout_4.addWidget(self.label)
+
+        self.chapter_table = QTableWidget(Widget)
+        self.chapter_table.setObjectName("chapter_table")
+
+        self.verticalLayout_4.addWidget(self.chapter_table)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.label_3 = QLabel(Widget)
+        self.label_3.setObjectName("label_3")
+
+        self.horizontalLayout_3.addWidget(self.label_3)
+
+        self.radio_no_convert = QRadioButton(Widget)
+        self.radio_no_convert.setObjectName("radio_no_convert")
+
+        self.horizontalLayout_3.addWidget(self.radio_no_convert)
+
+        self.radio_convert_epub = QRadioButton(Widget)
+        self.radio_convert_epub.setObjectName("radio_convert_epub")
+
+        self.horizontalLayout_3.addWidget(self.radio_convert_epub)
+
+        self.radio_convert_pdf = QRadioButton(Widget)
+        self.radio_convert_pdf.setObjectName("radio_convert_pdf")
+
+        self.horizontalLayout_3.addWidget(self.radio_convert_pdf)
+
+        self.radio_convert_cbz = QRadioButton(Widget)
+        self.radio_convert_cbz.setObjectName("radio_convert_cbz")
+
+        self.horizontalLayout_3.addWidget(self.radio_convert_cbz)
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout.addLayout(self.verticalLayout_4)
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.label_progress = QLabel(Widget)
+        self.label_progress.setObjectName("label_progress")
+
+        self.horizontalLayout_5.addWidget(self.label_progress)
+
+        self.progress_bar = QProgressBar(Widget)
+        self.progress_bar.setObjectName("progress_bar")
+        self.progress_bar.setValue(24)
+
+        self.horizontalLayout_5.addWidget(self.progress_bar)
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_5)
+
+        self.button_start = QPushButton(Widget)
+        self.button_start.setObjectName("button_start")
+
+        self.verticalLayout_2.addWidget(self.button_start)
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", "Widget", None))
+        self.label_4.setText(
+            QCoreApplication.translate("Widget", "Source Folder / URL:", None)
+        )
+        self.button_from_url.setText(
+            QCoreApplication.translate("Widget", "Search URL", None)
+        )
+        self.button_from_folder.setText(
+            QCoreApplication.translate("Widget", "Open Folder", None)
+        )
+        self.label_5.setText(QCoreApplication.translate("Widget", "Save to:", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Widget", "Browse", None))
+        self.label_2.setText(QCoreApplication.translate("Widget", "Metadata", None))
+        self.label.setText(QCoreApplication.translate("Widget", "Chapters", None))
+        self.label_3.setText(QCoreApplication.translate("Widget", "Convert?", None))
+        self.radio_no_convert.setText(QCoreApplication.translate("Widget", "No", None))
+        self.radio_convert_epub.setText(
+            QCoreApplication.translate("Widget", "EPUB", None)
+        )
+        self.radio_convert_pdf.setText(
+            QCoreApplication.translate("Widget", "PDF", None)
+        )
+        self.radio_convert_cbz.setText(
+            QCoreApplication.translate("Widget", "CBZ", None)
+        )
+        self.label_progress.setText(
+            QCoreApplication.translate("Widget", "Downloading...", None)
+        )
+        self.button_start.setText(QCoreApplication.translate("Widget", "Start!", None))
+
+    # retranslateUi
---
+++ mandown/ui/ui.py
@@ -0,0 +1,230 @@
+# pylint: disable=no-member,invalid-name,pointless-string-statement
+"""
+Generate mainwin.py while in mandown/ui/ with
+`uic -g python -o mainwin.py form.ui`
+"""
+import sys
+from pathlib import Path
+
+import requests
+from PySide6.QtGui import QImage, QPixmap
+
+import mandown
+from mandown import __version_str__
+from mandown.comic import BaseComic
+
+from .mainwin import Ui_Widget
+
+from PySide6.QtWidgets import (  # isort: skip
+    QApplication,
+    QFileDialog,
+    QMessageBox,
+    QTableWidgetItem,
+    QWidget,
+)
+
+
+COVER_IMG_HEIGHT = 400
+
+
+class MandownQtUi(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
+        self.init_hooks()
+
+        self.ui.progress_bar.setDisabled(True)
+        self.ui.progress_bar.setValue(0)
+        self.ui.label_progress.setText("Waiting")
+
+        self.ui.label_metadata.setWordWrap(True)
+
+        self.setWindowTitle(f"Mandown {__version_str__}")
+
+        self.source_path: str | None = None
+        self.source_url: str | None = None
+        self.dest_path: str | None = None
+        self.chapters = None
+        self._comic: BaseComic | None = None
+        self.img_cover: QImage | None = None
+
+        self.show()
+
+    def init_hooks(self) -> None:
+        # select "No" for default convert
+        self.ui.radio_no_convert.toggle()
+        # button: Open Folder
+        self.ui.button_from_folder.clicked.connect(self.hook_from_folder)
+        # button: Search URL
+        self.ui.button_from_url.clicked.connect(self.hook_from_url)
+        # button: Browse
+        self.ui.pushButton_3.clicked.connect(self.hook_set_dest)
+        # button: Start!
+        self.ui.button_start.clicked.connect(self.hook_go)
+
+    def metadata_to_table(self) -> list[str]:
+        metadata = self.comic.metadata
+        return [
+            f"<b>Title:</b> {metadata.title}",
+            f"<b>Author(s):</b> {', '.join(metadata.authors)}",
+            f"<b>Genre(s):</b> {', '.join(metadata.genres)}",
+            "",
+            metadata.description,
+        ]
+
+    def chapters_to_table(self) -> list:
+        # TODO: make them VIEWS / LAYOUTS not widgets
+        # because i can't add checkboxes
+        return [c.title for c in self.comic.chapters]
+
+    @property
+    def comic(self) -> BaseComic | None:
+        return self._comic
+
+    @comic.setter
+    def comic(self, comic: BaseComic) -> None:
+        self._comic = comic
+
+        self.ui.label_metadata.setText("<br />".join(self.metadata_to_table()))
+
+        for i, chap in enumerate(self.comic.chapters):
+            self.ui.chapter_table.setItem(i, 0, QTableWidgetItem(""))
+            self.ui.chapter_table.setItem(i, 1, QTableWidgetItem(chap.title))
+
+        # refresh screen w/metadata et al here
+        if comic.metadata.cover_art:
+            r = requests.get(comic.metadata.cover_art)
+            self.img_cover = QPixmap(
+                QImage.fromData(r.content).scaledToHeight(COVER_IMG_HEIGHT)
+            )
+            self.ui.label_image.setPixmap(self.img_cover)
+
+    @property
+    def text_dest_full(self) -> str:
+        return str(Path(self.ui.text_dest.text()) / self.comic.metadata.title)
+
+    """
+    Hooks go here!
+    """
+
+    def hook_from_folder(self) -> None:
+        # TODO: directly look for md-metadata.json instead
+        self.source_path = QFileDialog.getExistingDirectory(
+            self, "Open Comic Folder", "~"
+        )
+        self.ui.text_source.setText(self.source_path)
+        self.ui.text_dest.setText(str(Path(self.source_path).parent))
+
+        # load metadata
+        try:
+            self.comic = mandown.read(self.source_path)
+        except FileNotFoundError:
+            res = QMessageBox.critical(
+                self,
+                "Invalid Folder",
+                "Folder is not a Mandown-created comic folder: Could not find md-metadata.json.",
+                QMessageBox.Ok,
+            )
+
+    def hook_from_url(self) -> None:
+        active_url = self.ui.text_source.text()
+        try:
+            comic = mandown.query(active_url)
+        except ValueError:
+            res = QMessageBox.critical(
+                self,
+                "Unknown Comic",
+                "Could not find comic: URL did not match any sources.",
+                QMessageBox.Ok,
+            )
+        self.comic = comic
+
+    def hook_set_dest(self) -> None:
+        self.dest_path = QFileDialog.getExistingDirectory(
+            self, "Set Destination Folder", "~"
+        )
+        self.ui.text_dest.setText(self.dest_path)
+
+    def hook_go(self) -> None:
+        if not self.ui.text_source.text():
+            # if empty
+            res = QMessageBox.critical(
+                self,
+                "Missing source",
+                "Please select a source folder or URL",
+                QMessageBox.Ok,
+            )
+
+        if not self.ui.text_dest.text():
+            # if empty
+            res = QMessageBox.critical(
+                self,
+                "Missing destination",
+                "Please select a destination directory.",
+                QMessageBox.Ok,
+            )
+            return
+
+        if not self.comic:
+            res = QMessageBox.critical(
+                self,
+                "Something went wrong",
+                "ERROR: if not self.comic check failed. You should never see this message.",
+            )
+            return
+
+        # heavy bits
+        self.ui.label_progress.setText("Downloading")
+        max_size = len(self.comic.chapters)
+        self.ui.progress_bar.setDisabled(False)
+        for i, _ in enumerate(
+            mandown.download_progress(
+                self.comic,
+                self.ui.text_dest.text(),
+                threads=4,
+            ),
+            start=1,
+        ):
+            # TODO: this hangs the program so move it to a QThread
+            self.ui.progress_bar.setValue(int(i / max_size * 100))
+
+        # convert
+        if not self.ui.radio_no_convert.isChecked():
+            target = "none"
+            if self.ui.radio_convert_cbz.isChecked():
+                target = "cbz"
+            elif self.ui.radio_convert_epub.isChecked():
+                target = "epub"
+            elif self.ui.radio_convert_pdf.isChecked():
+                target = "pdf"
+
+            self.ui.label_progress.setText("Converting")
+            self.ui.progress_bar.setValue(1)
+            for i, text in enumerate(
+                mandown.convert_progress(
+                    self.comic,
+                    self.text_dest_full,
+                    target,
+                    self.text_dest_full,
+                ),
+                start=1,
+            ):
+                self.ui.label_progress.setText(text)
+                self.ui.progress_bar.setValue(int(i / max_size * 100))
+
+        res = QMessageBox.information(
+            self, "Done", "All operations complete.", QMessageBox.Ok
+        )
+
+        self.ui.progress_bar.setDisabled(True)
+
+
+def main() -> None:
+    app = QApplication([])
+    wid = MandownQtUi()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
