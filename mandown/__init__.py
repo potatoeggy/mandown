31cff7793b045174142ab7056c3070e3f960df34
eggy <eggyrules@gmail.com>
2022-08-14 19:29:12 -0400

    release: version bump 1.0.0

--- mandown/__init__.py
+++ mandown/__init__.py
@@ -1,13 +1,2 @@
-from .api import (  # isort: skip
-    convert,
-    convert_progress,
-    download,
-    download_progress,
-    load,
-    process,
-    process_progress,
-    query,
-)
-
-__version__ = (0, 10, 0)
+__version__ = (1, 0, 0)
 __version_str__ = ".".join(map(str, __version__))
