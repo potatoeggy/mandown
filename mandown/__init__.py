ff25f8f352ed9e9f2fd07275ff136182a1711508
eggy <eggyrules@gmail.com>
2022-08-16 22:45:12 -0400

    fix(api): add back imports
    pylance keeps deleting them >:(

--- mandown/__init__.py
+++ mandown/__init__.py
@@ -1,2 +1,26 @@
+from .api import (  # isort: skip
+    convert,
+    convert_progress,
+    download,
+    download_progress,
+    load,
+    process,
+    process_progress,
+    query,
+)
+
+# stupid pylance deleting my imports
+# pylint: disable=pointless-statement
+(
+    convert,
+    convert_progress,
+    download,
+    download_progress,
+    load,
+    process,
+    process_progress,
+    query,
+)
+
 __version__ = (1, 0, 0)
 __version_str__ = ".".join(map(str, __version__))
