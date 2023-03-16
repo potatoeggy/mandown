7b26b77b61dd5478b7ba56ea276f1da9b6284298
eggy <danielchen04@hotmail.ca>
2023-03-16 14:28:56 -0400

    chore: add mangasee test

---
+++ tests/test_source_mangasee.py
@@ -0,0 +1,27 @@
+import mandown
+
+KAGUYA_DESCRIPTION = "Kaguya Shinomiya and Miyuki Shirogane are the members of the incredibly prestigious Shuichi'in Academy's student council, asserting their positions as geniuses among geniuses. All the time they spend together has caused the two of them to develop feelings for each other, but their pride will not allow them to be the one to confess and become the submissive one in the relationship! Love is war, and their battle to make the other confess begins now!"
+
+
+def test_kaguya() -> None:
+    kaguya_url = "https://mangasee123.com/manga/Kaguya-Wants-To-Be-Confessed-To"
+
+    comic = mandown.query(kaguya_url)
+
+    expected_res = {
+        "title": "Kaguya-sama - Love Is War",
+        "authors": ["AKASAKA Aka"],
+        "genres": [
+            "Comedy",
+            "Drama",
+            "Romance",
+            "School Life",
+            "Seinen",
+            "Slice of Life",
+        ],
+        "description": KAGUYA_DESCRIPTION,
+        "cover_art": "https://temp.compsci88.com/cover/Kaguya-Wants-To-Be-Confessed-To.jpg",
+        "url": kaguya_url,
+    }
+
+    assert comic.metadata.asdict() == expected_res
