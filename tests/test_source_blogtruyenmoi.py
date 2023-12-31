ecb6aa0995c2732b1dc1d83ad62105abe788236d
eggy <danielchen04@hotmail.ca>
2023-12-31 00:27:30 -0500

    fix(blogtruyenmoi): fix description and author

---
+++ tests/test_source_blogtruyenmoi.py
@@ -0,0 +1,33 @@
+from common import skip_in_ci
+
+import mandown
+
+URL = "https://blogtruyenmoi.com/9600/kobayashi-san-chi-no-maid-dragon"
+REDUCED_URL = "https://blogtruyenmoi.com/9600"
+COVER_URL = "https://i7.xem-truyen.com/manga/9/9600/834dff95-65b3-46ed-be59-b246a5663b1b.thumb_500x.jpg"
+DESCRIPTION = "truyện kể về nhân vật Kobayashi - một nữ kĩ sư phần mềm sống một mình ở một khu chung cư. Trong một lần say tí bỉ, cô tình cờ cứu giúp một con rồng tên là Tooru. Có nợ thì phải trả, cô rồng Tooru quyết tâm sẽ giúp đỡ ân nhân của mình dưới vai trò là một hầu gái."
+
+
+@skip_in_ci
+def test_kobayashi() -> None:
+    comic = mandown.query(URL)
+
+    expected_res = {
+        "title": "Kobayashi-san Chi no Maid Dragon",
+        "authors": ["Cool Kyoushinja"],
+        "genres": [
+            "Comedy",
+            "Seinen",
+            "Slice of life",
+            "Ecchi",
+            "Drama",
+            "Fantasy",
+            "Yuri",
+        ],
+        "description": DESCRIPTION,
+        "cover_art": COVER_URL,
+        "url": REDUCED_URL,
+    }
+
+    assert comic.metadata.asdict() == expected_res
+    assert comic.chapters
