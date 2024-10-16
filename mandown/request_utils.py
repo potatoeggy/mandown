e7179c551be8302b7f0e30c66fb5209e3c009888
eggy <d7chen@uwaterloo.ca>
2024-10-15 22:12:18 -0400

    chore: add more timeouts

--- mandown/request_utils.py
+++ mandown/request_utils.py
@@ -6,4 +6,4 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 class requests:
     @staticmethod
     def get(url: str) -> RealRequests.Response:
-        return RealRequests.get(url, headers={"User-Agent": USER_AGENT})
+        return RealRequests.get(url, headers={"User-Agent": USER_AGENT}, timeout=5)
